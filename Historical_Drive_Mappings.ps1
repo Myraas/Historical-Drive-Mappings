b236f83781ec1a2c8a4cb25139dab52ae84518fb
Kody <34889710+Myraas@users.noreply.github.com>
2024-08-27 14:35:04 -0700

    Update HistoricalDriveMappings.ps1

--- Historical_Drive_Mappings.ps1
+++ Historical_Drive_Mappings.ps1
@@ -1,34 +1,39 @@
-Function Get-CurrentDriveMaps{
-
-	Write-Host "Current Drive Mappings" -ForegroundColor Green
-	$MappedDrives = Get-WmiObject -Query "SELECT Caption, ProviderName FROM Win32_MappedLogicalDisk" | Select-Object @{ Name = 'DriveLetter'; Expression = { $_.Caption } }, @{ Name = 'NetworkPath'; Expression = { $_.ProviderName } } | format-table -Property @{ e='DriveLetter'; width = 2}, NetworkPath -hidetableheaders
-	
-	if ($MappedDrives -eq $null){
-		""
-		Write-Host "No drives are currently mapped" -ForegroundColor Red
-		""
-	} else {
-		Get-WmiObject -Query "SELECT Caption, ProviderName FROM Win32_MappedLogicalDisk" | Select-Object @{ Name = 'DriveLetter'; Expression = { $_.Caption } }, @{ Name = 'NetworkPath'; Expression = { $_.ProviderName } } | format-table -Property @{ e='DriveLetter'; width = 2}, NetworkPath -hidetableheaders
-	}
+Function Get-ActiveDriveMaps {
+    Write-Host "Current Drive Mappings" -ForegroundColor Green
+    $MappedDrives = Get-WmiObject -Query "SELECT Caption, ProviderName FROM Win32_MappedLogicalDisk" | 
+                    Select-Object @{ Name = 'DriveLetter'; Expression = { $_.Caption } }, 
+                                  @{ Name = 'NetworkPath'; Expression = { $_.ProviderName } } | 
+                    Format-Table -Property @{ e='DriveLetter'; width = 2 }, NetworkPath -HideTableHeaders
+    
+    if ($null -eq $MappedDrives) {
+        ""
+        Write-Host "No drives are currently mapped" -ForegroundColor Red
+        ""
+    } else {
+        $MappedDrives
+    }
 }
-Function Get-HistoricalDriveMaps{
 
-	Write-Host "Historical Drive Mappings" -ForegroundColor Green
-	$HistoricalDrives = Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\*" | Where-Object { $_.Name -like "*##*" } | ForEach-Object { ( Split-Path -Path $_.Name -Leaf ).Replace("#", "\") }
-	
-	if($HistoricalDrives -eq $null){
-		""
-		Write-Host "No historical drive maps were found" -ForegroundColor Red
-		""
-	} else {
-		""
-		Write-Host $HistoricalDrives
-		""
-		""
-	}
+function Get-PreviousDriveMaps {
+    Write-Host "Previous Drive Mappings" -ForegroundColor Green
+    $HistoricalDrives = Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\*" | 
+                        Where-Object { $_.Name -like "*##*" } | 
+                        ForEach-Object { (Split-Path -Path $_.Name -Leaf).Replace("#", "\") }
+    
+    if ($null -eq $HistoricalDrives -or $HistoricalDrives.Count -eq 0) {
+        ""
+        Write-Host "No previous drive maps were found" -ForegroundColor Red
+        ""
+    } else {
+        ""
+        foreach ($Drive in $HistoricalDrives) {
+            Write-Host $Drive
+        }
+        ""
+    }
 }
 
 Clear-Host
-Get-CurrentDriveMaps
-Get-HistoricalDriveMaps
+Get-ActiveDriveMaps
+Get-PreviousDriveMaps
 Pause
